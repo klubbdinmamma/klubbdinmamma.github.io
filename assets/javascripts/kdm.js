211eaf9dc6fc2aa1eb2c0ccd9b33f3883e967375
Patrik Ragnarsson <patrik@starkast.net>
2016-06-26 22:27:08 +0200

    deploy 4ac8c2a

--- assets/javascripts/kdm.js
+++ assets/javascripts/kdm.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
         $(this).toggleClass('active',
             $(this).find('a').attr('href') == window.location.pathname);
     });
+
+    $('.facebook-photos').each(function() {
+      $(this).magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+          enabled: true
+        }
+      });
+    });
 });
