f961e940e8cf461bbf0b9a987d31b94a036053fa
Patrik Ragnarsson <patrik@starkast.net>
2016-07-01 20:25:54 +0200

    be able to pick facebook albums for display

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source "https://rubygems.org"
 
 gem "jekyll-bootstrap-sass"
 gem "koala", "~> 2.2"
+
+group :development do
+  gem "pry"
+end
--- _plugins/render_facebook_photos.rb
+++ _plugins/render_facebook_photos.rb
@@ -9,17 +9,20 @@ class RenderFacebookPhotos < Liquid::Tag
   def render(context)
     config = context.registers[:site].config
 
-    app_id         = config['facebook']['app_id']
-    app_secret     = config['facebook']['app_secret']
-    callback_url   = config['facebook']['callback_url']
-    username       = config['facebook']['username']
-    exclude_albums = config['facebook']['exclude_albums']
+    app_id         = config.fetch("facebook").fetch("app_id")
+    app_secret     = config.fetch("facebook").fetch("app_secret")
+    callback_url   = config.fetch("facebook").fetch("callback_url")
+    username       = config.fetch("facebook").fetch("username")
+    exclude_albums = config.fetch("facebook").fetch("exclude_albums_from_download")
+    include_albums = config.fetch("facebook").fetch("include_albums_in_html")
 
     fb = FacebookPhotos.new(app_id, app_secret, callback_url, username, exclude_albums)
 
     html = ""
 
     fb.albums.each do |album|
+      next unless include_albums.include?(album.name)
+
       html << "<div class='facebook-photos'>\n"
       html << "  <h2>#{album.name}</h2>\n"
 
